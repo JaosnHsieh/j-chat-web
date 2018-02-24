2455742a8e654cc956cf4441a27ecfca8e28e2c7
JaosnHsieh <appleoxxo@gmail.com>
2018-02-24 14:36:40 +0800

    add user list and message list data in Contacts Component

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,96 +1,141 @@
 import React from "react";
-import faker from "faker";
+// import faker from "faker";
 import { Link } from "react-router-dom";
 // import Chat from "./Chat.js";
-const list = [
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
-  },
-  {
-    id: faker.random.uuid(),
-    avatar: `https://placem.at/people?w=100`,
-    name: faker.name.findName()
+import axios from "../../libs/axios.js";
+
+class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userList: [],
+      groupList: [],
+      messages: []
+    };
   }
-];
-const Contacts = ({ match }) => (
-  <div>
-    {list.map(ele => (
-      <div key={ele.id}>
-        <Link to={`${match.url}/${ele.id}`}>
-          <img src={ele.avatar} alt={"123"} />
-          {ele.name}
-        </Link>
+  async componentDidMount() {
+    const [{ data: users }, { data: messages }] = await Promise.all([
+      axios.get("/user"),
+      axios.get("/message/user")
+    ]);
+    this.setState({
+      userList: users.map(user => {
+        user.avatar = `https://placem.at/people?w=100`;
+        return user;
+      }),
+      messages: messages.reduce((result, ele) => {
+        return {
+          ...result,
+          [ele.ChatMessage.idno]: result[`${ele.ChatMessage.idno}`]
+            ? [...result[ele.ChatMessage.idno], ele.ChatMessage]
+            : [ele.ChatMessage]
+        };
+      }, {})
+    });
+  }
+  render() {
+    const { match } = this.props;
+    const { userList, messages } = this.state;
+    console.log("messages in render method", messages);
+    return (
+      <div>
+        {userList.map(ele => (
+          <div key={ele.idno}>
+            <Link to={`${match.url}/${ele.idno}`}>
+              <span>
+                {(messages[ele.idno] && messages[ele.idno].length) || 0}
+              </span>
+
+              <img src={ele.avatar} alt={"123"} />
+              {ele.name}
+            </Link>
+          </div>
+        ))}
+        {/* <Route exact path={match.path} render={() => <h2>select topic la</h2>} /> */}
       </div>
-    ))}
-    {/* <Route exact path={match.path} render={() => <h2>select topic la</h2>} /> */}
-  </div>
-);
+    );
+  }
+}
+
+// const list = [
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   },
+//   {
+//     id: faker.random.uuid(),
+//     avatar: `https://placem.at/people?w=100`,
+//     name: faker.name.findName()
+//   }
+// ];
+// const Contacts = ({ match }) => (
+
+// );
 
 export default Contacts;
