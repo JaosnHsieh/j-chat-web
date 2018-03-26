0542ef9fd16b61d5ebe36151a21b32ae033992e8
JaosnHsieh <appleoxxo@gmail.com>
2018-03-26 16:39:19 +0100

    ssr works

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,1 +1,185 @@
-"use strict";var precacheConfig=[["/index.html","ee10d6ac1c6375a9f29640788611584e"],["/static/css/main.558b0555.css","008c31413c7a5f0fd80abdab926878b2"],["/static/js/main.519288ad.js","bfff754d9a266be7400dde539de4cafc"],["/static/media/jchat-logo.8028768c.png","8028768c67bc76bcf032309413c61478"],["/static/media/team-icon.4dd9258d.png","4dd9258d4f850183ed0ee0ec99612927"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(t){return new Response(t,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,t){var n=new URL(e);return n.hash="",n.search=n.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return t.every(function(t){return!t.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),n.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(t){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(n){if(!t.has(n)){var r=new Request(n,{credentials:"same-origin"});return fetch(r).then(function(t){if(!t.ok)throw new Error("Request for "+n+" returned a response with status "+t.status);return cleanResponse(t).then(function(t){return e.put(n,t)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var t=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(n){return Promise.all(n.map(function(n){if(!t.has(n.url))return e.delete(n)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var t,n=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching),r="index.html";(t=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),t=urlsToCacheKeys.has(n));var a="/index.html";!t&&"navigate"===e.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],e.request.url)&&(n=new URL(a,self.location).toString(),t=urlsToCacheKeys.has(n)),t&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(t){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,t),fetch(e.request)}))}});
+"use strict";
+var precacheConfig = [
+    ["/index.html", "c0afa4982384b5b0ff91a99f148b3ce7"],
+    ["/static/css/main.558b0555.css", "008c31413c7a5f0fd80abdab926878b2"],
+    ["/static/js/main.ee00f8a8.js", "685427b1af7c2a8e683e435f7ca4df67"],
+    [
+      "/static/media/jchat-logo.8028768c.png",
+      "8028768c67bc76bcf032309413c61478"
+    ],
+    ["/static/media/team-icon.4dd9258d.png", "4dd9258d4f850183ed0ee0ec99612927"]
+  ],
+  cacheName =
+    "sw-precache-v3-sw-precache-webpack-plugin-" +
+    (self.registration ? self.registration.scope : ""),
+  ignoreUrlParametersMatching = [/^utm_/],
+  addDirectoryIndex = function(e, t) {
+    var n = new URL(e);
+    return "/" === n.pathname.slice(-1) && (n.pathname += t), n.toString();
+  },
+  cleanResponse = function(e) {
+    return e.redirected
+      ? ("body" in e ? Promise.resolve(e.body) : e.blob()).then(function(t) {
+          return new Response(t, {
+            headers: e.headers,
+            status: e.status,
+            statusText: e.statusText
+          });
+        })
+      : Promise.resolve(e);
+  },
+  createCacheKey = function(e, t, n, r) {
+    var a = new URL(e);
+    return (
+      (r && a.pathname.match(r)) ||
+        (a.search +=
+          (a.search ? "&" : "") +
+          encodeURIComponent(t) +
+          "=" +
+          encodeURIComponent(n)),
+      a.toString()
+    );
+  },
+  isPathWhitelisted = function(e, t) {
+    if (0 === e.length) return !0;
+    var n = new URL(t).pathname;
+    return e.some(function(e) {
+      return n.match(e);
+    });
+  },
+  stripIgnoredUrlParameters = function(e, t) {
+    var n = new URL(e);
+    return (
+      (n.hash = ""),
+      (n.search = n.search
+        .slice(1)
+        .split("&")
+        .map(function(e) {
+          return e.split("=");
+        })
+        .filter(function(e) {
+          return t.every(function(t) {
+            return !t.test(e[0]);
+          });
+        })
+        .map(function(e) {
+          return e.join("=");
+        })
+        .join("&")),
+      n.toString()
+    );
+  },
+  hashParamName = "_sw-precache",
+  urlsToCacheKeys = new Map(
+    precacheConfig.map(function(e) {
+      var t = e[0],
+        n = e[1],
+        r = new URL(t, self.location),
+        a = createCacheKey(r, hashParamName, n, /\.\w{8}\./);
+      return [r.toString(), a];
+    })
+  );
+function setOfCachedUrls(e) {
+  return e
+    .keys()
+    .then(function(e) {
+      return e.map(function(e) {
+        return e.url;
+      });
+    })
+    .then(function(e) {
+      return new Set(e);
+    });
+}
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(function(e) {
+        return setOfCachedUrls(e).then(function(t) {
+          return Promise.all(
+            Array.from(urlsToCacheKeys.values()).map(function(n) {
+              if (!t.has(n)) {
+                var r = new Request(n, { credentials: "same-origin" });
+                return fetch(r).then(function(t) {
+                  if (!t.ok)
+                    throw new Error(
+                      "Request for " +
+                        n +
+                        " returned a response with status " +
+                        t.status
+                    );
+                  return cleanResponse(t).then(function(t) {
+                    return e.put(n, t);
+                  });
+                });
+              }
+            })
+          );
+        });
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
+}),
+  self.addEventListener("activate", function(e) {
+    var t = new Set(urlsToCacheKeys.values());
+    e.waitUntil(
+      caches
+        .open(cacheName)
+        .then(function(e) {
+          return e.keys().then(function(n) {
+            return Promise.all(
+              n.map(function(n) {
+                if (!t.has(n.url)) return e.delete(n);
+              })
+            );
+          });
+        })
+        .then(function() {
+          return self.clients.claim();
+        })
+    );
+  }),
+  self.addEventListener("fetch", function(e) {
+    if ("GET" === e.request.method) {
+      var t,
+        n = stripIgnoredUrlParameters(
+          e.request.url,
+          ignoreUrlParametersMatching
+        ),
+        r = "index.html";
+      (t = urlsToCacheKeys.has(n)) ||
+        ((n = addDirectoryIndex(n, r)), (t = urlsToCacheKeys.has(n)));
+      var a = "/index.html";
+      !t &&
+        "navigate" === e.request.mode &&
+        isPathWhitelisted(["^(?!\\/__).*"], e.request.url) &&
+        ((n = new URL(a, self.location).toString()),
+        (t = urlsToCacheKeys.has(n))),
+        t &&
+          e.respondWith(
+            caches
+              .open(cacheName)
+              .then(function(e) {
+                return e.match(urlsToCacheKeys.get(n)).then(function(e) {
+                  if (e) return e;
+                  throw Error(
+                    "The cached response that was expected is missing."
+                  );
+                });
+              })
+              .catch(function(t) {
+                return (
+                  console.warn(
+                    'Couldn\'t serve response for "%s" from cache: %O',
+                    e.request.url,
+                    t
+                  ),
+                  fetch(e.request)
+                );
+              })
+          );
+    }
+  });
